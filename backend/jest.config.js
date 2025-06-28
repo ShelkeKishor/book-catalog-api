ac15a12a8cc76755f785b29698331f4395d6f668
ShelkeKishor <hemrajshelke087@gmail.com>
2025-06-28 18:49:44 +0530

    fix: update jest.config.js for ESM compatibility

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,13 +1,12 @@
 export default {
   testEnvironment: 'node',
   transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  moduleNameMapper: {},
   setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'clover'],
   testMatch: ['**/__tests__/**/*.test.js'],
   verbose: true,
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
-  }
 };
