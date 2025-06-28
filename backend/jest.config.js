5af911865b4f3f023fd0aaf624921234cede184d
ShelkeKishor <hemrajshelke087@gmail.com>
2025-06-28 20:16:13 +0530

    Fix Jest configuration for ES modules support

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,12 +1,14 @@
 export default {
   testEnvironment: 'node',
-  transform: {},
   extensionsToTreatAsEsm: ['.js'],
-  moduleNameMapper: {},
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'clover'],
   testMatch: ['**/__tests__/**/*.test.js'],
   verbose: true,
+  testTimeout: 10000,
 };
